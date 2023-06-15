d3451b36e3643f107d1c2d2d98b573093389dd1a
ludsonfelipe <felipe_ludson@hotmail.com>
2023-06-14 21:59:53 -0300

    function deployed

--- source/crypto_api_function/main.py
+++ source/crypto_api_function/main.py
@@ -1,17 +1,14 @@
 import requests as re
-import json
 from google.cloud import secretmanager
+from google.cloud import pubsub_v1
 import os
 from time import sleep
-from google.cloud import pubsub_v1
-
+import json
 
 def get_bitcoin_data(request):
     topic_id = os.environ.get('TOPIC_ID')
     project_id = os.environ.get('PROJECT_ID')
     secret_id = os.environ.get('SECRET_ID')
-
-    secret = get_secret_value(secret_id, project_id)
 
     publisher = pubsub_v1.PublisherClient()
     topic_path = publisher.topic_path(project_id, topic_id)
@@ -22,18 +19,19 @@ def get_bitcoin_data(request):
             'currency':'USD',
             'sort':'rank',
             'order':'ascending',
-            'limit':100,
+            'limit':10,
             'meta':True
             })
 
     headers = {
-    'x-api-key':secret,
+    'x-api-key':secret_id,
     'content-type': 'application/json'
     }
-
-    while True:
+    value = 0
+    while value<10:
         sleep(1)
         response = re.request("POST", url, headers=headers, data=payload)
+        print(response)
         publisher.publish(topic_path, response.text.encode("utf-8"))
-    
+        value +=1
     return "Requisição foi um sucesso!"
--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-project_id="my_project_id"
+project_id="playground-s-11-c3a318ad"
 
 gcloud config set project $project_id
 gcloud services enable secretmanager.googleapis.com
+gcloud services enable cloudfunctions.googleapis.com
+gcloud services enable cloudbuild.googleapis.com
